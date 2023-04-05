69b082b2646346591bed94dcc23182828ad7812c
Ramon Wanderley <ramonwanderley13@gmail.com>
2023-04-05 12:16:43 -0300

    implement Navbar initial version

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,5 +1,6 @@
 import Router from './Router';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from "./components/navbar";
 
-ReactDOM.render( <ChakraProvider><Router /></ChakraProvider>, document.getElementById('root'));
+ReactDOM.render( <ChakraProvider><Navbar/><Router /></ChakraProvider>, document.getElementById('root'));
