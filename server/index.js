b6172d1d58da387faf8a6bdcf58dc180beced378
Ramon Wanderley <ramonwanderley13@gmail.com>
2023-04-05 09:38:07 -0300

    implement favorite feature routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const port = 3000
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require('./favorites')(app)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
