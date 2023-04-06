62cc737b7b3d759d1c37a8572631da6b6c838df1
lmm3 <lmm3@cin.ufpe.br>
2023-04-06 06:14:21 -0300

    changed search route to server index.js and created a database class

--- server/index.js
+++ server/index.js
@@ -5,12 +5,28 @@ const port = 3000
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const JSONDatabase = require('./JSONDatabase');
+
+const db = new JSONDatabase('./samples/users.json');
+
 require('./favorites')(app)
-require('./search')(app);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+app.get('/search', (req, res) => {
+  const query = req.query.q;
+  if (query) {
+      console.log(req.query.q);
+      const results = db.getAllMatchingNames(query);
+      res.json(results);
+  } else {
+      const allRecords = db.getAll();
+      res.json(allRecords);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
