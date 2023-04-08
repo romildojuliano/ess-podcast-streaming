cacde8a54fff30d7ed2e88da818f74351ed7e0ad
lmm3 <lmm3@cin.ufpe.br>
2023-04-08 17:02:20 -0300

    showing user results in search page

--- web-app/src/pages/Search/index.tsx
+++ web-app/src/pages/Search/index.tsx
@@ -3,34 +3,31 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 
+let query;
+let users;
+
+async function getData() {
+  const response =  await fetch(`http://localhost:4000/search?q=${query}`);
+  const data =  await response.json()
+  users = data;
+  return data;
+}
+
+
 function Search(){
   const location = useLocation();
-  const query = new URLSearchParams(location.search).get('q');
-
-  let users = [];
-
-  async function getData() {
-  
-    const response = await fetch(`http://localhost:4000/search?q=${query}`);
-    const data = await response.json()
-    console.log(data);
-    users = data;
-    return (data);
-  
-  }
+  query = new URLSearchParams(location.search).get('q');
 
   getData();
+ 
+  let jsonUsers = JSON.stringify(users)
+  console.log(jsonUsers)
 
   return (
     <div>
     <h2>Search Results for: {query}</h2>
-      {users.map((user, index) => (
-        <div  key={index}>
-          <p>Username: {user}</p>
-        </div>
-      ))}
+    <p>{jsonUsers}</p>  
     </div>
-
   );
 }
 
