fad7228b715b1077127ed8a70de1c8060c78846c
joaovictorbelo <jvblb@cin.ufpe.br>
2023-04-08 01:56:14 -0300

    installed cors in the server

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const port = 4000
+const cors = require('cors');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const JSONDatabase = require('./JSONDatabase');
 
