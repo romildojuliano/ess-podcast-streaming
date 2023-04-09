139fea16939f91286b47f53c5e2b07a1180c820b
lmm3 <lmm3@cin.ufpe.br>
2023-04-09 10:32:35 -0300

    fixed search issue

--- web-app/src/pages/Search/index.tsx
+++ web-app/src/pages/Search/index.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+// import React from 'react';
+// import { useState, useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
 
+
+
+
+// async function waitForData(){
+//   await getData();
+// }
+
+// async function getData() {
+//   const response =  await fetch(`http://localhost:4000/search?q=${query}`);
+//   const data =  await response.json()
+//   users = data;
+//   return data;
+// }
+
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 let query;
 let users;
 
-async function getData() {
-  const response =  await fetch(`http://localhost:4000/search?q=${query}`);
-  const data =  await response.json()
-  users = data;
-  return data;
-}
-
-
 function Search(){
   const location = useLocation();
-  query = new URLSearchParams(location.search).get('q');
+  const [users, setUsers] = useState([]);
 
-  getData();
- 
+  useEffect(() => {
+    async function waitForData(){
+      const query = new URLSearchParams(location.search).get('q');
+      const response =  await fetch(`http://localhost:4000/search?q=${query}`);
+      const data =  await response.json()
+      setUsers(data);
+    }
+    waitForData(); 
+  }, [location.search]);
+
   let jsonUsers = JSON.stringify(users)
   console.log(jsonUsers)
 
@@ -30,6 +46,25 @@ function Search(){
     </div>
   );
 }
+
+// export default Search;
+
+
+// function Search(){
+//   const location = useLocation();
+//   query = new URLSearchParams(location.search).get('q');
+
+//   waitForData(); 
+//   let jsonUsers = JSON.stringify(users)
+//   console.log(jsonUsers)
+
+//   return (
+//     <div>
+//     <h2>Search Results for: {query}</h2>
+//     <p>{jsonUsers}</p>  
+//     </div>
+//   );
+// }
 
 
 export default Search;
