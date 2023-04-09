d4d86e7863eff42066ecb7b7c2186cbaa7037115
mattvie <mvmf@cin.ufpe.br>
2023-04-09 13:35:54 -0300

    Added ChannelPage

--- web-app/src/Router.tsx
+++ web-app/src/Router.tsx
@@ -1,24 +1,37 @@
-import { UserPage, Home, PodcastPage, Explore, Politicsseemore, Economyseemore, Politicsseemore2, Search, FollowingPage, RegisterPage} from './pages';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
+import {
+  UserPage,
+  ChannelPage,
+  Home,
+  PodcastPage,
+  Explore,
+  Politicsseemore,
+  Economyseemore,
+  Politicsseemore2,
+  Search,
+  FollowingPage,
+  RegisterPage,
+} from "./pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
 function Router() {
   return (
-      <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          {/*adicionem aqui as rotas das paginas */}
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/user/:username" element={<UserPage />} />
-          <Route path="/podcast" element={<PodcastPage />} />
-          <Route path="/following" element={<FollowingPage />} />
-          <Route path="/explore" element={<Explore/>} />
-          <Route path="/search" element={<Search/>} />
-          <Route path="/Politicsseemore" element={<Politicsseemore />} />
-          <Route path="/Economyseemore" element={<Economyseemore/>} />
-          <Route path='/Politicsseemore2' element={<Politicsseemore2/>} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        {/*adicionem aqui as rotas das paginas */}
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/user/:username" element={<UserPage />} />
+        <Route path="/channel/:author" element={<ChannelPage />} />
+        <Route path="/podcast" element={<PodcastPage />} />
+        <Route path="/following" element={<FollowingPage />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/Politicsseemore" element={<Politicsseemore />} />
+        <Route path="/Economyseemore" element={<Economyseemore />} />
+        <Route path="/Politicsseemore2" element={<Politicsseemore2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
