686f87c9dfa37fb0e6fa72ed36eb7edc8f6def67
romildojuliano <rjal@cin.ufpe.br>
2023-04-11 10:01:13 -0300

    (feat) router now redirects to login if not logged; login and register screen added

--- web-app/src/pages/RegisterPage/index.tsx
+++ web-app/src/pages/RegisterPage/index.tsx
@@ -1,9 +1,77 @@
-import { Box, Text } from "@chakra-ui/layout";
+import { Center, Image, Input, Box, Button, FormControl, Stack, Text, VStack, Link } from "@chakra-ui/react";
+import podshare_logo from '../../styles/assets/podshare-logo.svg';
+// import { Link } from "react-router-dom";
 
-export default function RegisterPage () {
+const border = "0px";
+
+function RegisterGroup () {
     return (
-        <Box>
-            <Text>Hello World!</Text>
+        <Stack spacing="2" border={border} w="15%">
+            {/* Usuário */}
+            <Text align="center" fontWeight="bold">Usuário</Text>    
+            <FormControl alignContent="center" alignItems="center" alignSelf="center" marginTop="15%">
+                <Input type="text" placeholder="E-mail" border={border} size="sm" variant="filled" />
+            </FormControl>
+            
+            {/* E-mail */}
+            <Text align="center" fontWeight="bold">E-mail</Text>
+            <FormControl>
+                <Input type="email" placeholder="E-mail" border={border} size="sm" variant="filled" />
+            </FormControl>
+
+            {/* Senha */}
+            <Text align="center" fontWeight="bold">Senha</Text>
+            <FormControl>
+                <Input type="password" placeholder="Senha" border={border} size="sm" variant="filled"/> 
+            </FormControl>
+
+            {/* Senha */}
+            <Text align="center" fontWeight="bold">Data de nascimento</Text>
+            <FormControl>
+                <Input type="date" placeholder="Data de nascimento" border={border} size="sm" variant="filled"/> 
+            </FormControl>
+
+            <Button 
+                colorScheme="orange"
+                type="submit" 
+                variant="solid" 
+                marginTop="10%" 
+                marginLeft="35%" 
+                w="60%" 
+                alignSelf="center"
+            >Criar conta</Button>
+        </Stack>
+    );
+}
+
+export default function RegisterPage() {
+    return (
+        <Box alignContent="center">
+            <VStack>
+                <Image src={podshare_logo} alt="podshare logo" border={border} marginTop="8%"/>
+                <RegisterGroup />
+            </VStack>
+            <Center>
+                <Link marginTop=".75%" href="/l">↵ Voltar para tela de login </Link>
+            </Center>
         </Box>
+
+    //     <Center marginTop="8%">
+    //     <Center>
+    //         <Image src={podshare_logo} alt="podshare logo" border="2px"/>
+    //     </Center>
+    //     <Center 
+    //         border="2px" 
+    //         borderColor="red" 
+    //         display='flex'
+    //         bg='tomato'
+    //         h='100px'
+    //     >
+    //         <LoginGroup /> 
+    //     </Center>
+    //     <Button colorScheme="orange" type="submit" variant="solid">
+    //         Login
+    //     </Button>
+    // </Center>
     );
 }
