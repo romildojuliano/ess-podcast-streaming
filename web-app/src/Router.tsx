62e79384c805f26969e6ad81584a653b6246d9da
romildojuliano <rjal@cin.ufpe.br>
2023-04-11 17:49:33 -0300

    (fix) Router now working on routes

--- web-app/src/Router.tsx
+++ web-app/src/Router.tsx
@@ -1,6 +1,5 @@
 import {
   UserPage,
-  Home,
   FavoritesPage,
   PodcastPage,
   ChannelPage,
@@ -13,55 +12,63 @@ import {
   FollowingPage,
   RegisterPage
 } from "./pages";
-import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
 
 function Router() {
-  const user = localStorage.getItem("user");
-  console.log(user)
-  if (!user) {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />}/>
-        </Routes>
-      </BrowserRouter>
-    )
-  }
+  //const user = localStorage.getItem("user");
+  // console.log(user)
+  // if (!user) {
+  //   return (
+  //     <BrowserRouter>
+  //       <Routes>
+  //         <Route path="/*" element={<Navigate to="/login" />} />
+  //         <Route path="/login" element={<LoginPage />} />
+  //         <Route path="/register" element={<RegisterPage />}/>
+  //       </Routes>
+  //     </BrowserRouter>
+  //   )
+  // }
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    var aux = localStorage.getItem('user')
+    setUser(aux === null? '' : aux);
+  }, [user])
 
   return (
-    <BrowserRouter>
-      {
-        localStorage.getItem("user") == null ? 
-        <Routes>
-          <Route path="/*" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />}/>
-        </Routes>
-      :
-      <>
-        <Navbar />
-        <Routes>
-          {/*adicionem aqui as rotas das paginas */}
-          <Route path="/*" element={<Navigate to="/" />} /> 
-          <Route path="/" element={<Home />} />
-          <Route path="/user/:username" element={<UserPage />} />
-          <Route path="/podcast/:podcast" element={<PodcastPage />} />
-          <Route path="/channel/:username" element={<ChannelPage />} />
-          <Route path="/following" element={<FollowingPage />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/Politicsseemore" element={<Politicsseemore />} />
-          <Route path="/Economyseemore" element={<Economyseemore />} />
-          <Route path="/Politicsseemore2" element={<Politicsseemore2 />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
-        </Routes>
-      </>
-      }
+    // <HashRouter basename="/">
       
+    // </HashRouter>
+
+    <BrowserRouter>
+        {
+          user === '' ?
+          <Routes>
+            <Route path="/*" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} /> 
+          </Routes>
+          :
+        <>
+          <Navbar />
+          <Routes>
+            {/*adicionem aqui as rotas das paginas */}
+            <Route path="/*" element={<Navigate to="/explore"/> } /> 
+            <Route path="/user/:username" element={<UserPage />} />
+            <Route path="/podcast/:podcast" element={<PodcastPage />} />
+            <Route path="/channel/:username" element={<ChannelPage />} />
+            <Route path="/following" element={<FollowingPage />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/Politicsseemore" element={<Politicsseemore />} />
+            <Route path="/Economyseemore" element={<Economyseemore />} />
+            <Route path="/Politicsseemore2" element={<Politicsseemore2 />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+          </Routes>
+        </>
+
+        }
+        
     </BrowserRouter>
   );
 }
