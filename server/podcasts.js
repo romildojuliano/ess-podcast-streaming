7a09d265734bd52ed64e53f15bc98b0a70f54e18
mattvie <mvmf@cin.ufpe.br>
2023-04-11 16:42:11 -0300

    ChannelPage duplicated podcasts POST handled

--- server/podcasts.js
+++ server/podcasts.js
@@ -3,23 +3,23 @@ var fs = require('fs');
 module.exports = function (app) {
 
     app.post('/podcasts', (req, res) => {
+        var data = JSON.parse(fs.readFileSync('./samples/podcasts.json', 'utf8'))
         if (req.body.subject == "") {
-            res.status(400).send('Invalid Request (Subject Missing').end();
+            res.status(400).send('Invalid Request (Subject Missing)').end();
         } else if (req.body.name == "") {
-            res.status(400).send('Invalid Request (Name Missing').end();
+            res.status(400).send('Invalid Request (Name Missing)').end();
         } else if (req.body.link == "") {
-            res.status(400).send('Invalid Request (Link Missing').end();
+            res.status(400).send('Invalid Request (Link Missing)').end();
         } else if (req.body.author == "") {
-            res.status(400).send('Invalid Request (Author Missing').end();
+            res.status(400).send('Invalid Request (Author Missing)').end();
+        } else if (data.filter(x => x.name == req.body.name).length > 0) {
+            res.status(400).send('Invalid Request (Duplicated Podcast)').end();
         } else {
 
-            var data = JSON.parse(fs.readFileSync('./samples/podcasts.json', 'utf8'))
-
+            // extracting the thumbnail if it is a youtube link
             var link = req.body.link;
             //var link = "https://www.youtube.com/watch?v=3Hp-yUDSF8g";
-
             var link_url, image_code;
-
             if (link.length == 43) {
                 link_url = link.slice(0, 23)
                 image_code = link.slice(32)
